676b3a6d56962762116ee22705d2c783657bbed6
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-21 16:31:48 +0200

    fix(DatabaseConfig): restructured file

--- src/main/java/Task6/HerokunateAPI/Utils/DatabaseConfig.java
+++ src/main/java/Task6/HerokunateAPI/Utils/DatabaseConfig.java
@@ -7,7 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import javax.sql.DataSource;
 import com.zaxxer.hikari.*;
 
+
 /*This class is going to be run on its own when the app starts to config*/
+
 @Configuration
 public class DatabaseConfig {
 
