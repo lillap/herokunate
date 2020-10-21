37af4676b49056fbfa4ae72a9fe0c513c7f7e5d1
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-21 15:25:41 +0200

    :tada: initial commit

---
+++ src/main/java/Task6/HerokunateAPI/HerokunateApplication.java
@@ -0,0 +1,13 @@
+package Task6.HerokunateAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokunateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokunateApplication.class, args);
+	}
+
+}
