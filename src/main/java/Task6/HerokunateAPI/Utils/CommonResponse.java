4e4e1ae5b657b62a2b4b69f03fb58754a1a7119f
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-22 14:02:13 +0200

    feat(CommonResponse):Added class and properties

---
+++ src/main/java/Task6/HerokunateAPI/Utils/CommonResponse.java
@@ -0,0 +1,7 @@
+package Task6.HerokunateAPI.Utils;
+
+/*A layer above Entityresponse to add something extra to the response*/
+public class CommonResponse {
+    public Object data;
+    public String message;
+}
