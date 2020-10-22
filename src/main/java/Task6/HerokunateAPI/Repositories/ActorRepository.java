afc786a9d530e7239d8206e93eccb7d0328f89db
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-22 09:28:22 +0200

    fix(ActorRepository): import Actor class

--- src/main/java/Task6/HerokunateAPI/Repositories/ActorRepository.java
+++ src/main/java/Task6/HerokunateAPI/Repositories/ActorRepository.java
@@ -1,6 +1,6 @@
 package Task6.HerokunateAPI.Repositories;
 
-
+import Task6.HerokunateAPI.Models.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActorRepository extends JpaRepository<Actor, Long> {
