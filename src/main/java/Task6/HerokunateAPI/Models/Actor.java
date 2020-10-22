03ac0771177655a9f368656570780910506b5254
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-22 14:14:18 +0200

    feat(ActorModel): add getters and setters

--- src/main/java/Task6/HerokunateAPI/Models/Actor.java
+++ src/main/java/Task6/HerokunateAPI/Models/Actor.java
@@ -30,5 +30,43 @@ public class Actor {
     @Column
     private String url;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
