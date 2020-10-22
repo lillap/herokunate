3f9e31cb080809a80417e5c697bc6201a566bb2e
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-22 14:10:40 +0200

    feat(ActorController): addActor method

--- src/main/java/Task6/HerokunateAPI/Controllers/ActorController.java
+++ src/main/java/Task6/HerokunateAPI/Controllers/ActorController.java
@@ -2,7 +2,9 @@ package Task6.HerokunateAPI.Controllers;
 
 import Task6.HerokunateAPI.Models.Actor;
 import Task6.HerokunateAPI.Repositories.ActorRepository;
+import Task6.HerokunateAPI.Utils.CommonResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,9 +21,17 @@ public class ActorController {
 
 
     @PostMapping("/actor")
-    public ResponseEntity<> addActor(HttpServletRequest request, HttpServletResponse response,
-                                     @RequestBody Actor actor) {
+    public ResponseEntity<CommonResponse> addActor(HttpServletRequest request, HttpServletResponse response,
+                                                   @RequestBody Actor actor) {
+        actor = actorRepository.save(actor);
 
+        CommonResponse commonResponse = new CommonResponse();
+
+        commonResponse.data = actor;
+        commonResponse.message = "New actor with id: " + actor.id;
+
+        HttpStatus httpStatus = HttpStatus.CREATED;
+        
 
     }
 
