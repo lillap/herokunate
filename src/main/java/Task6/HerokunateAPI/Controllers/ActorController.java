b01d92b71b4a161f4d7e84b1c2e3506d42d3df0a
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-22 13:21:47 +0200

    feat(ActorController): added autowired actorRepository

--- src/main/java/Task6/HerokunateAPI/Controllers/ActorController.java
+++ src/main/java/Task6/HerokunateAPI/Controllers/ActorController.java
@@ -1,8 +1,14 @@
 package Task6.HerokunateAPI.Controllers;
 
+import Task6.HerokunateAPI.Repositories.ActorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ActorController {
+
+    @Autowired
+    private ActorRepository actorRepository;
     
+
 }
