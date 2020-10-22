6a9bd60dfa17bf0df6f842423ac64f11480c6e6d
NORDIC\pkarim <paria.karim@gmail.com>
2020-10-22 13:49:47 +0200

    feat(ActorController): AddActor method skeleton

--- src/main/java/Task6/HerokunateAPI/Controllers/ActorController.java
+++ src/main/java/Task6/HerokunateAPI/Controllers/ActorController.java
@@ -1,14 +1,28 @@
 package Task6.HerokunateAPI.Controllers;
 
+import Task6.HerokunateAPI.Models.Actor;
 import Task6.HerokunateAPI.Repositories.ActorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class ActorController {
 
     @Autowired
     private ActorRepository actorRepository;
-    
+
+
+    @PostMapping("/actor")
+    public ResponseEntity<> addActor(HttpServletRequest request, HttpServletResponse response,
+                                     @RequestBody Actor actor) {
+
+
+    }
 
 }
